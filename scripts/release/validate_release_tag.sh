faefee9388e82db66e2017c859773e3d8647f803
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2024-02-22 06:45:17 +0100

    review fix

--- scripts/release/validate_release_tag.sh
+++ scripts/release/validate_release_tag.sh
@@ -7,7 +7,7 @@ set -o pipefail
 
 CURRENT_RELEASE_TAG=$1
 
-semver_pattern="^[0-9]+\.[0-9]+\.[0-9]+(-[a-z][a-z0-9]*)?$"
+semver_pattern="^([0-9]|[1-9][0-9]*)[.]([0-9]|[1-9][0-9]*)[.]([0-9]|[1-9][0-9]*)(-[a-z][a-z0-9]*)?$"
 
 if ! [[ $CURRENT_RELEASE_TAG =~ $semver_pattern ]]; then
     echo "Given tag \"$CURRENT_RELEASE_TAG\" does not match the expected semantic version pattern: \"$semver_pattern\"."
